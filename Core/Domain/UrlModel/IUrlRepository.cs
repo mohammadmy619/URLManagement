34d4723a0bacb34b4dfd801e657b47041c55fa76
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 08:51:26 +0330

    implementation Url Entity In the domain layer

---
+++ Core/Domain/UrlModel/IUrlRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UrlModel
+{
+    public interface IUrlRepository
+    {
+        public Task<Url?> GetAuthorByIdAsync(Guid UrlId, CancellationToken cancellationToken);
+        public Task<Guid> AddUrlAsync(Url url, CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Core/Domain/UrlModel/Url.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UrlModel
+{
+    public class Url:BaseEntity
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+
+
+    }
+}
