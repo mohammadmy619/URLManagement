94c7957668597f7321f1e9d691abd138bd66ebe6
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 19:56:39 +0330

    add DbContext Fixture in test

---
+++ Test/TestUrl/Fixtures/DbContextFixture.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Migrations;
+
+
+namespace TestLibrary.Fixtures;
+
+public class DbContextFixture : EfDatabaseBaseFixture<UrlManagementDbContext>
+{
+    protected override UrlManagementDbContext BuildDbContext(DbContextOptions<UrlManagementDbContext> options)
+    {
+        return new UrlManagementDbContext(options);
+    }
+}
