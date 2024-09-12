b4eaf915750532e0708144a1811e3b710a5a8e41
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 09:45:45 +0330

    create UrlModel Configuration

---
+++ Infrastructure/Persistence/Configurations/UrlModelConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.UrlModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class UrlModelConfiguration : IEntityTypeConfiguration<Url>
+    {
+        public void Configure(EntityTypeBuilder<Url> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.description).HasMaxLength(400).IsRequired(false);
+        }
+    }
+}
