c2da071afeb9164146714c8a781361e0c99332ac
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 11:52:23 +0330

    Create Url Command Handler  and implement Validation

---
+++ Core/Application/Urls/AddUrlCommand/CreateUrlCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.AddUrlCommand
+{
+    public class CreateUrlCommandHandler(IUrlRepository UrlRepository) : IRequestHandler<CreateUrlCommand, CreateUrlCommandResponse>
+    {
+        private readonly IUrlRepository _UrlRepository = UrlRepository;
+
+        public async Task<CreateUrlCommandResponse> Handle(CreateUrlCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _UrlRepository.AddUrlAsync((Url)request, cancellationToken);
+
+            await _UrlRepository.SaveChangesAsync(cancellationToken);
+            return new CreateUrlCommandResponse(result);
+        }
+    }
+}
---
+++ Core/Application/Urls/AddUrlCommand/CreateUrlCommandValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.AddUrlCommand
+{
+    public class CreateUrlCommandValidation : AbstractValidator<CreateUrlCommand>
+    {
+        public CreateUrlCommandValidation()
+        {
+            RuleFor(a => a.Path)
+             .NotEmpty().WithMessage("{Path} is required")
+             .NotNull()
+             .MaximumLength(400).WithMessage("{Path} must not exceed 400 characters");
+
+            RuleFor(a => a.Name)
+            .NotEmpty().WithMessage("{Name} is required")
+             .NotNull()
+             .MaximumLength(100).WithMessage("{Name} must not exceed 100 characters");
+
+            RuleFor(a => a.description)
+         .NotEmpty().WithMessage("{description} is required")
+          .MaximumLength(400).WithMessage("{description} must not exceed 400 characters");
+
+
+        }
+    }
+}
