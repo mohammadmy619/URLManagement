fed2f675871a63aeffaa3ff7194bb2e62569a241
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 11:26:51 +0330

    Create  Command and Response for Url entity

---
+++ Core/Application/Urls/AddUrlCommand/CreateUrlCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.AddUrlCommand
+{
+
+    public record CreateUrlCommand(string Path, string Name, string description) : IRequest<CreateUrlCommandResponse>
+    {
+
+        public static explicit operator Url(CreateUrlCommand Url)
+    => new Url()
+    {
+        Path = Url.Path,
+        Name = Url.Name,
+        description = Url.description,
+        IsDeleted = false,
+
+    };
+    };
+}
---
+++ Core/Application/Urls/AddUrlCommand/CreateUrlCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.AddUrlCommand
+{
+    public record CreateUrlCommandResponse(Guid UrlId)
+    {
+        public static explicit operator CreateUrlCommandResponse(Guid BookId)
+           => new CreateUrlCommandResponse(BookId);
+    }
+}
