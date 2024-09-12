e2038fb6c1d555c0288dca83864b5363c47832e4
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 12:16:42 +0330

    create Url Query and response Class

---
+++ Core/Application/Urls/GetUrlByIdQuery/GetUrlException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.GetUrlByIdQuery
+{
+    public class GetUrlException:Exception
+    {
+        private const string _messages = "Url with UrlId `{0}` not NotFound.";
+
+        public GetUrlException(Guid UrlId) : base(string.Format(_messages, UrlId)) { }
+    }
+  
+}
---
+++ Core/Application/Urls/GetUrlByIdQuery/GetUrlResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.GetUrlByIdQuery
+{
+    public record GetUrlResponse(Guid UrlId,string Path,string Name,string description)
+    {
+        public static explicit operator GetUrlResponse(Url Url)
+    => new GetUrlResponse(Url.Id, Url.Path, Url.Name, Url.description);
+    }
+}
