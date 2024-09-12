3df663060a4b7cfe8849b5baa3d50bc772ef9221
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 23:03:20 +0330

    Add   Validation Exception Filter

---
+++ Presentation/URl.Api/ValidationExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using FluentValidation;
+
+public class ValidationExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is ValidationException validationException)
+        {
+            var errorMessage = $"{validationException.Message}";
+
+            context.Result = new BadRequestObjectResult(new
+            {
+                Message = errorMessage
+            });
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
