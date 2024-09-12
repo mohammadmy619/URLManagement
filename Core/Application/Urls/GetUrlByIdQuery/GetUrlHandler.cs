cfc61b8ff96c90b2400d7681b6bf19ec4d0a8c43
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 12:23:51 +0330

    Create  Get Url Handler

---
+++ Core/Application/Urls/GetUrlByIdQuery/GetUrlHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Urls.GetUrlByIdQuery
+{
+    public class GetUrlHandler(IUrlRepository UrlRepository) : IRequestHandler<GetUrlByIdQuery, GetUrlResponse>
+    {
+        private readonly IUrlRepository _UrlRepository = UrlRepository;
+
+        public async Task<GetUrlResponse> Handle(GetUrlByIdQuery request, CancellationToken cancellationToken)
+        {
+            var Url = await _UrlRepository.GetAuthorByIdAsync(request.BookId, cancellationToken);
+
+
+            if (Url is null)
+            {
+                throw new GetUrlException(request.BookId);
+            }
+
+            return (GetUrlResponse)Url;
+        }
+    }
+}
