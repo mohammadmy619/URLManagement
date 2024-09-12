523dddbc98c7d58a2368c36db492a2c3cfe7b160
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 10:10:00 +0330

    Create UrlRepository

---
+++ Infrastructure/Persistence/Repositories/UrlRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.UrlModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UrlRepository(UrlManagementDbContext _dbContext) : IUrlRepository
+    {
+        public async Task<Guid> AddUrlAsync(Url url, CancellationToken cancellationToken)
+        {
+            // Add the url to the database asynchronously  
+            var result = await _dbContext.Url.AddAsync(url, cancellationToken);
+
+            // Return the Id of the newly created url  
+            return result.Entity.Id;
+        }
+
+        public async Task<Url?> GetAuthorByIdAsync(Guid UrlId, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Url
+                  .FirstOrDefaultAsync(author => author.Id == UrlId, cancellationToken);
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
