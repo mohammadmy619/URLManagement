035c5a0ba8c64a7a0edfaa3301776e1397903106
mohammad yousefi <mohammadmy619@gmail.com>
2024-09-12 10:18:42 +0330

    Add  Url model Configuration

--- Infrastructure/Persistence/Configurations/UrlModelConfiguration.cs
+++ Infrastructure/Persistence/Configurations/UrlModelConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Domain.UrlModel;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
